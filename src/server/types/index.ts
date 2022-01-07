fd9d2db1b8438c73b8f9eb112f3be3497d18842b
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-01-06 20:01:28 -0500

    first commit, initialized boilerplate

---
+++ src/server/types/index.ts
@@ -0,0 +1,41 @@
+import { Request } from 'express';
+import { UsersTable } from '../database/models';
+
+export interface MySQL_Default_Response {
+    insertId: number;
+    affectedRows: number;
+}
+
+// Add Books, Categories, Users, ReqUser
+
+export interface ReqUser extends Request {
+    user?: Users;
+}
+
+export interface Users {
+    id?: number;
+    name?: string;
+    email?: string;
+    password?: string;
+    _created?: string
+}
+
+export interface Books {
+    id?: number;
+    categoryid?: number;
+    title?: string;
+    author?: string;
+    price?: number;
+    _created?: string;
+
+}
+
+export interface Categories {
+    id?: number;
+    name: string;
+}
+
+export interface Payload extends UsersTable {
+    id?: number;
+    role?:number;
+}
